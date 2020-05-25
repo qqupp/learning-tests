7b3e340dcc187a54d616e73404f0394120be5fef
Paolo Picci <paolo.picci@springernature.com>
2020-05-25 12:11:35 +0100

    add traversable

--- src/main/scala/learning/codec/Decoder.scala
+++ src/main/scala/learning/codec/Decoder.scala
@@ -1,5 +1,8 @@
 package learning.codec
 
+import cats._
+import cats.implicits._
+
 /*
   Generalized decoder from A to B with Error E
  */
@@ -8,6 +11,19 @@ final case class Decoder[-A, +E, +B](decode: A => Either[E, B]) {
 
   def andThen[E1 >: E, C](decoder: Decoder[B, E1, C]): Decoder[A, E1, C] =
     Decoder { a => decode(a).flatMap( decoder.decode ) }
+
+  def ignoreError: Decoder[A, Nothing, Option[B]] =
+    Decoder { a => decode(a) match {
+      case Left(_) => Right(None)
+      case Right(b) => Right(Some(b))
+      }
+    }
+
+
+  def toTraversableList: Decoder[List[A], E, List[B]] = toTraversable[List, A, B]
+
+  def toTraversable[F[_] : Traverse, AA <: A, BB >: B]: Decoder[F[AA], E, F[BB]] =
+    Decoder( (traversable: F[AA]) => traversable.traverse(decode(_)))
 
   def map[C](f: B => C): Decoder[A, E, C] =
     Decoder { a => decode(a).map(f) }
@@ -28,4 +44,9 @@ final case class Decoder[-A, +E, +B](decode: A => Either[E, B]) {
 object Decoder {
   def success[T](value: T): Decoder[Any, Nothing, T] = Decoder(_ => Right(value))
   def fail[T](value: T): Decoder[Any, T, Nothing] = Decoder(_ => Left(value))
+
+  import cats._
+  import cats.implicits._
+  def traversingDecoder[F[_] : Traverse, A, E, B](decoder: Decoder[A, E, B]): Decoder[F[A], E, F[B]] =
+    Decoder(traversable => traversable.traverse(decoder(_)))
 }
--- src/test/scala/learning/codec/DecoderSpec.scala
+++ src/test/scala/learning/codec/DecoderSpec.scala
@@ -1,5 +1,6 @@
 package learning.codec
 
+import cats.Id
 import org.scalatest.{FlatSpec, Matchers}
 
 import scala.util.Try
@@ -15,7 +16,9 @@ class DecoderSpec extends FlatSpec with Matchers {
   }
 
   it should "compose and shortcut errors" in {
-    val strng2PosInt: Decoder[String, Throwable, PosInt] = str2Int andThen int2PosInt
+    val fstDecoder: Decoder[String, Throwable, Int] = str2Int
+    val sndDecoder: Decoder[Int, PosIntException, PosInt] = int2PosInt
+    val strng2PosInt: Decoder[String, Throwable, PosInt] = fstDecoder andThen sndDecoder
 
     val successResult = strng2PosInt("2")
     successResult shouldBe Right(PosInt.unsafeFrom(2))
@@ -27,9 +30,31 @@ class DecoderSpec extends FlatSpec with Matchers {
     failSndConversion.left.get shouldBe a[PosIntException]
   }
 
+  it should "derive a traversableDecoder" in {
+    val ListStr2ListInt: Decoder[List[String], Throwable, List[Int]] = str2Int.toTraversableList
+
+    val csvStr2ListInt: Decoder[String, Throwable, List[Int]] = str2Csv andThen ListStr2ListInt
+
+    csvStr2ListInt("2,45,-13,35") shouldBe Right(List(2,45,-13,35))
+    csvStr2ListInt("2,45,Boom!,35").left.get shouldBe a[java.lang.NumberFormatException]
+  }
+
+  it should "remove the errors lifting the result to an option" in {
+    val noErrorDecoder: Decoder[String, Nothing, Option[Int]] = str2Int.ignoreError
+
+    noErrorDecoder("1") shouldBe Right(Some(1))
+    noErrorDecoder("asdf") shouldBe Right(None)
+  }
+
+  lazy val str2Csv: Decoder[String, Nothing, List[String]] =
+    Decoder( s => Right(s.split(",").toList) )
+
   lazy val str2Int: Decoder[String, Throwable, Int] =
     Decoder((str: String) => Try(str.toInt).toEither )
 
+  import cats._
+  import cats.implicits._
+  lazy val lxxx: Decoder[List[String], Throwable, List[Int]] = Decoder(l => l.traverse(str2Int(_)))
 
   class PosIntException(msg: String) extends Exception(msg)
   sealed abstract case class PosInt(x: Int)
