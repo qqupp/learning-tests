8bedb6fdbd4ba43825edfa0ed9aa27bbece6e184
Paolo Picci <paolo.picci@springernature.com>
2020-05-25 09:37:59 +0100

    add composition

--- src/test/scala/learning/codec/DecoderSpec.scala
+++ src/test/scala/learning/codec/DecoderSpec.scala
@@ -11,9 +11,33 @@ class DecoderSpec extends FlatSpec with Matchers {
     failedResult shouldBe a[Left[_, _]]
 
     val successResult = str2Int("2")
-    successResult shouldBe a[Right[_, _]]
+    successResult shouldBe Right(2)
+  }
+
+  it should "compose and shortcut errors" in {
+    val strng2PosInt: Decoder[String, Throwable, PosInt] = str2Int andThen int2PosInt
+
+    val successResult = strng2PosInt("2")
+    successResult shouldBe Right(PosInt.unsafeFrom(2))
+
+    val failFstConversion = strng2PosInt("asdf")
+    failFstConversion.left.get shouldBe a[java.lang.NumberFormatException]
+
+    val failSndConversion = strng2PosInt("-10")
+    failSndConversion.left.get shouldBe a[PosIntException]
   }
 
   lazy val str2Int: Decoder[String, Throwable, Int] =
     Decoder((str: String) => Try(str.toInt).toEither )
+
+
+  class PosIntException(msg: String) extends Exception(msg)
+  sealed abstract case class PosInt(x: Int)
+  object PosInt {
+    def from(i: Int): Either[PosIntException, PosInt] =
+      Either.cond(i > 0, new PosInt(i) {}, new PosIntException(s"cant build a pos int for $i"))
+
+    def unsafeFrom(i: Int): PosInt = from(i).right.get
+  }
+  lazy val int2PosInt: Decoder[Int, PosIntException, PosInt] = Decoder((i: Int) => PosInt.from(i) )
 }
