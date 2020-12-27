dabb91ae28b8e96244f568591c1b26be67c687e7
Paolo Picci <paolo.picci@springernature.com>
2020-12-27 15:27:41 +0000

    back to tuples

--- src/main/scala/learning/Tuplelist.scala
+++ src/main/scala/learning/Tuplelist.scala
@@ -27,28 +27,24 @@ object TupleList extends App {
   // 3  Interpreting Typed Higher-Order Languages
 
   sealed trait Var[Env, T]
-  case class VZ[E, T]()                extends Var[Env[T, E], T]
-  case class VS[A, E, T](v: Var[E, T]) extends Var[Env[A, E], T]
+  case class VZ[E, T]()                extends Var[(T, E), T]
+  case class VS[A, E, T](v: Var[E, T]) extends Var[(A, E), T]
 
-  sealed trait Env[H, T]
-  case class EnvCons[H, T <: Env[_, _]](h: H, t: T) extends Env[H, T]
-  case class EnvNil()                               extends Env[Unit, Nothing]
-
-  implicit class EnvOps[T <: Env[_, _]](val t: T) extends AnyVal {
-    def :::[H](h: H): Env[H, T] = EnvCons(h, t)
+  implicit class EnvOps[T](val t: T) extends AnyVal {
+    def :::[H](h: H): Tuple2[H, T] = (h, t)
   }
 
-  def lookp[H, T, E, O](v: Var[E, O], env: Env[H, T])(implicit ev: E =:= Env[H, T]): O =
+  def lookp[H, T, E, O](v: Var[E, O], env: (H, T))(implicit ev: E =:= Tuple2[H, T]): O =
     (v, env) match {
-      case (VZ(), EnvCons(h, _))            => h.asInstanceOf[O]
-      case (vv: VS[a, e, t], EnvCons(_, t)) => lookp(vv.v.asInstanceOf[Var[Env[a, e], t]], t.asInstanceOf[Env[a, e]])
-      case _                                => ???
+      case (VZ(), (h, _))            => h.asInstanceOf[O]
+      case (vv: VS[a, e, t], (_, t)) => lookp(vv.v.asInstanceOf[Var[Tuple2[a, e], t]], t.asInstanceOf[Tuple2[a, e]])
+      case _                         => ???
     }
 
-  val myEnv: Env[String, Env[Int, Env[Char, EnvNil]]]                = "one" ::: 2 ::: '3' ::: EnvNil()
-  val myVar01: Var[Env[String, Env[Int, Env[Char, EnvNil]]], String] = VZ()
-  val myVar02: Var[Env[String, Env[Int, Env[Char, EnvNil]]], Int]    = VS(VZ())
-  val myVar03: Var[Env[String, Env[Int, Env[Char, EnvNil]]], Char]   = VS(VS(VZ()))
+  val myEnv: (String, (Int, (Char, Unit)))                = "one" ::: 2 ::: '3' ::: ()
+  val myVar01: Var[(String, (Int, (Char, Unit))), String] = VZ()
+  val myVar02: Var[(String, (Int, (Char, Unit))), Int]    = VS(VZ())
+  val myVar03: Var[(String, (Int, (Char, Unit))), Char]   = VS(VS(VZ()))
 
   val result01: String = lookp(myVar01, myEnv)
   val result02: Int    = lookp(myVar02, myEnv)
